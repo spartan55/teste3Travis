ebd40a0cb841f7d3efd5e850c3351100368e125b
spartan55 <joeu123@gmail.com>
2016-09-01 15:07:13 -0300

    Update HelloWorld.java

--- src/cc/engeld/HelloWorld.java
+++ src/cc/engeld/HelloWorld.java
@@ -2,6 +2,17 @@ package cc.engeld;
 
 public class HelloWorld {
     public static void main(String Args[]) {
-        System.out.println("Hello world!!");
+        Gerador g = new Gerador();
+        Integer[] numeros = g.gerarNumerosAleatorios(10, 100);
+        
+        for (Integer i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i] + "\n");
+        }
+        SortElements testeOrdenacao = new Bubble();
+        testeOrdenacao.sortItems(numeros);
+        System.out.println("Algoritmo Ordenacao\n");
+        for (Integer i = 0; i < numeros.length; i++) {
+            System.out.println(numeros[i] + "\n");
+        }
     }
 }
